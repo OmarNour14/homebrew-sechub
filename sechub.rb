7d84bd62786ecf5c417394cf11eed0ca6560b7dd
Omarnour-el.din <omarnour-el.din@Omars-MacBook-Pro.local>
2024-05-15 11:10:32 +0100

    updating the ruby script

--- sechub.rb
+++ sechub.rb
@@ -1,8 +1,8 @@
 class Sechub < Formula
     desc "CLI tool to update notes in AWS Security Hub"
     homepage "https://github.com/omarnour14/homebrew-sechub"  # Update with your actual project URL
-    url "https://github.com/omarnour14/homebrew-sechub/archive/v.1.0.0.tar.gz"  # URL to the tar.gz of the release
-    sha256 "77146eb8dbad2747e4c9399ac9f26a161831828c1e61f68f856d656a0a5aea73"  # Generate this with `shasum -a 256 filename`
+    url "https://github.com/OmarNour14/homebrew-sechub/archive/refs/tags/v1.0.0.tar.gz"  # URL to the tar.gz of the release
+    sha256 "2476f3d3144bf2781262d5af2f4f1dcb52bf4fabde98b13248f11ae1d59c7340"  # Generate this with `shasum -a 256 filename`
   
     def install
       bin.install "sechub"
