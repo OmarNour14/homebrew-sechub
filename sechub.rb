8e1c7078e3f2fbb2b6058bd97980cb00233b5bc3
Omarnour-el.din <omarnour-el.din@Omars-MacBook-Pro.local>
2024-05-15 11:38:45 +0100

    updating the ruby file

--- sechub.rb
+++ sechub.rb
@@ -5,7 +5,7 @@ class Sechub < Formula
     sha256 "2476f3d3144bf2781262d5af2f4f1dcb52bf4fabde98b13248f11ae1d59c7340"  # Generate this with `shasum -a 256 filename`
   
     def install
-        bin.install "homebrew-sechub-1.0.0/sechub.sh" => "sechub"
+        bin.install "./sechub.sh" => "sechub"
       end
   
     test do
